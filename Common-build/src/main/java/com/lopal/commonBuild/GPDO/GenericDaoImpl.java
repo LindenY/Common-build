89f2ef22ec577daf51acf8fe1bbe1a3f2ab6add8
DesLin <desmond.zq.lin@gmail.com>
2014-03-10 12:00:55 -0400

    Changed to springframework.orm.hibernate4

--- Common-build/src/main/java/com/lopal/commonBuild/GPDO/GenericDaoImpl.java
+++ Common-build/src/main/java/com/lopal/commonBuild/GPDO/GenericDaoImpl.java
@@ -4,37 +4,41 @@ import java.util.List;
 
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 
-public class GenericDaoImpl<T extends DomainObject> extends HibernateDaoSupport implements GenericDao<T> {
+public class GenericDaoImpl<T extends DomainObject> implements GenericDao<T> {
 	
 	private Class<T> type;
+	private SessionFactory sessionFactory;
 	
 	public GenericDaoImpl(Class<T> type) {
 		super();
 		this.type = type;
 	}
 	
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
 	@Autowired
-	public void setupSessionFactory(SessionFactory sessionFactory) {
+	public void setSessionFactory(SessionFactory sessionFactory) {
 		this.setSessionFactory(sessionFactory);
 	}
 
 	public T get(Long id) {
-		return (T) getHibernateTemplate().get(type, id);
+		return (T) this.sessionFactory.getCurrentSession().get(type, id);
 	}
 	
 	public List<T> getAll() {
-		return getHibernateTemplate().loadAll(type);
+		return null;
 	}
 	
 	public void save(T object) {
-		getHibernateTemplate().save(object);
-		getHibernateTemplate().flush();
+		this.sessionFactory.getCurrentSession().save(object);
+		this.sessionFactory.getCurrentSession().flush();
 	}
 	
 	public void delete(T object) {
-		getHibernateTemplate().delete(object);
+		this.sessionFactory.getCurrentSession().delete(object);
 	}
 	
 }
